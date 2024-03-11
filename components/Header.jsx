aa1e73ae7516fd6f26c2b35e81348db46e0dbac0
Nayeem <nayeem2281998@gmail.com>
2024-03-11 21:10:01 +0530

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,22 @@
+import '../styles/Header.css'
+
+export default function Header() {
+  return (
+    <div>
+      <div className='black_header'>
+        <div className='black_header_iconDiv '>
+        <img src='/fourBoxLogo.png' alt="four box logo of black header" className='w-4 h-4' />
+        <p>Lorem ipsum dolor</p>
+        </div>
+        <div className='black_header_iconDiv'>
+        <img src='/fourBoxLogo.png' alt="four box logo of black header" className='w-4 h-4' />
+        <p>Lorem ipsum dolor</p>
+        </div>
+        <div className='black_header_iconDiv'>
+        <img src='/fourBoxLogo.png' alt="four box logo of black header" className='w-4 h-4' />
+        <p>Lorem ipsum dolor</p>
+        </div>
+      </div>
+    </div>
+  );
+}
